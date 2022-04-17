f11c1da8fc15f1dbb4e56321eed64cf5d7ebd24f
MianOxair <MianUzair.CA@gmail.com>
2022-04-16 19:36:00 -0700

    My Fourth Commit worked on CSS Nav Bar

--- my-app/src/Components/Comp_Head.js
+++ my-app/src/Components/Comp_Head.js
@@ -5,9 +5,12 @@ function Comp_Head()
         <nav>
             <ul>
                 <h1>Welcome</h1>
-                <a className="Comp_Head-link1" href="http://www.google.com">Google</a>
-                <a className="Comp_Head-link2" href="http://www.youtube.com">Youtube</a>
-                <a className="Comp_Head-link3" href="http://www.facebook.com">facebook</a>
+                <li><a className="Comp_Head-link1" href="http://www.google.com">Google</a></li>
+                <li><a className="Comp_Head-link2" href="http://www.youtube.com">Youtube</a></li>
+                <li><a className="Comp_Head-link3" href="http://www.facebook.com">facebook</a></li>
+                <li><a className="Comp_Head-link4" href="http://www.twitter.com">Twitter</a></li>
+                <li><a className="Comp_Head-link5" href="http://www.wikipedia.org">Wikipedia</a></li>
+                <li><a className="Comp_Head-link6" href="http://www.instagram.com">Instagram</a></li>
             </ul>
         </nav>
         </header>
